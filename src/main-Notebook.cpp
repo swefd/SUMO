9b0c7e1ae732a00f592015976cb7197d09323fa1
Just swefd <smile.lol007@gmail.com>
2021-06-05 18:42:11 +0300

    test

--- src/main-Notebook.cpp
+++ src/main-Notebook.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <IRremote.h>
 
 //--Motor Settings --
 #define maxSpeed 255   // Максимальна Швидкість
@@ -147,7 +148,7 @@ public:
   }
 };
 
-Car sumo; //Створення об'єкту машинки
+Car sumo;
 
 void setup()
 {
@@ -162,16 +163,39 @@ void setup()
   sumo.setLeftSide(rightA, rightB, speedRight);
   sumo.setRightSide(leftA, leftB, speedLeft);
   Serial.begin(9600);
-
-
-
-  
-
-
-  
+  IrReceiver.begin(4,0);
 }
 
 void loop()
 {
-  sumo.debugSensors();
+  //sumo.debugSensors();
+  
+  sumo.ringCheck();
+
+if (IrReceiver.decode()) {
+
+        // Print a short summary of received data
+        IrReceiver.printIRResultShort(&Serial);
+        if (IrReceiver.decodedIRData.protocol == UNKNOWN) {
+            // We have an unknown protocol here, print more info
+            IrReceiver.printIRResultRawFormatted(&Serial, true);
+        }
+        Serial.println();
+
+        /*
+         * !!!Important!!! Enable receiving of the next value,
+         * since receiving has stopped after the end of the current received data packet.
+         */
+        IrReceiver.resume(); // Enable receiving of the next value
+
+        /*
+         * Finally, check the received data and perform actions according to the received command
+         */
+        if (IrReceiver.decodedIRData.command == 0x10) {
+            // do something
+        } else if (IrReceiver.decodedIRData.command == 0x11) {
+            // do something else
+        }
+    }
+
 }
